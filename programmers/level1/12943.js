506aea9d777a9d135a835ac901b938fd9bee4702
Jihyun Lee <jihyunlee.me@gmail.com>
2022-10-03 23:54:39 +0900

    콜라스 추측 코드 정리, 메모사항

--- programmers/level1/12943.js
+++ programmers/level1/12943.js
@@ -17,3 +17,20 @@ function solution(num) {
     }
     return counter === 500 ? -1 : counter;
 }
+
+function solution(num) {
+    let counter = 0;
+    while(num != 1 && counter !== 500) {
+        num % 2 === 0 ? num = num / 2 : num = num * 3 + 1;
+        counter++
+    }
+    return counter === 500 ? -1 : counter;
+}
+
+/*
+line 9의 if문을 while문의 조건으로 넣을 수 있었는데 이 생각을 하지 못했다.
+while (true) {  
+    statement
+} 
+while문의 조건이 참일때 실행되면 statement 반복 수행
+*/
